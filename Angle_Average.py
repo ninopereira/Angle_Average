6358e971d7bafdb47dd337ff349534fd09f3eae6
Nino Pereira <nino.pereira@dyson.com>
2017-09-14 11:26:22 +0100

    feat: verifies length of input array. aditional verification tests.

--- Angle_Average.py
+++ Angle_Average.py
@@ -9,7 +9,10 @@ def to_deg(angle):
 
 # this list is a numpy array
 def average_angle(list_angles, angle_type = 'deg'):
-
+    if len(list_angles)<1:
+        return 'undefined'
+    if len(list_angles)<2:
+        return list_angles[0]
     if angle_type == 'deg':
         for i in range(len(list_angles)):
             list_angles[i] = to_rad(list_angles[i])
@@ -35,8 +38,73 @@ def assert_similar(num1,num2,tol):
     assert(num1>(num2-tol))
     assert(num1<(num2+tol))
 
-# TEST average_angle
-tol = 0.1
+# TESTS SUITE
+tol = 1
+tol = 1 # 1 degree tolerance
+
+# equivalences:
+# 25 = 205
+# 50 = 230
+# 155 = 335
+assert_similar (average_angle([25,   50, 155],'deg'), 16.7, tol)
+assert_similar (average_angle([50,  155, 205],'deg'), 16.7, tol)
+assert_similar (average_angle([155, 205, 230],'deg'), 16.7, tol)
+assert_similar (average_angle([205, 230, 335],'deg'), 16.7, tol)
+assert_similar (average_angle([230, 335,  25],'deg'), 16.7, tol)
+assert_similar (average_angle([335,  25,  50],'deg'), 16.7, tol)
+
+# 25 = 205
+assert_similar (average_angle([205,   50, 155],'deg'), 16.7, tol)
+assert_similar (average_angle([50,  155, 25],'deg'), 16.7, tol)
+assert_similar (average_angle([155, 25, 230],'deg'), 16.7, tol)
+assert_similar (average_angle([25, 230, 335],'deg'), 16.7, tol)
+assert_similar (average_angle([230, 335,  205],'deg'), 16.7, tol)
+assert_similar (average_angle([335,  205,  50],'deg'), 16.7, tol)
+
+# 50 = 230
+assert_similar (average_angle([25,   230, 155],'deg'), 16.7, tol)
+assert_similar (average_angle([230,  155, 205],'deg'), 16.7, tol)
+assert_similar (average_angle([155, 205, 50],'deg'), 16.7, tol)
+assert_similar (average_angle([205, 50, 335],'deg'), 16.7, tol)
+assert_similar (average_angle([50, 335,  25],'deg'), 16.7, tol)
+assert_similar (average_angle([335,  25,  230],'deg'), 16.7, tol)
+
+# 155 = 335
+assert_similar (average_angle([25,   50, 335],'deg'), 16.7, tol)
+assert_similar (average_angle([50,  335, 205],'deg'), 16.7, tol)
+assert_similar (average_angle([335, 205, 230],'deg'), 16.7, tol)
+assert_similar (average_angle([205, 230, 155],'deg'), 16.7, tol)
+assert_similar (average_angle([230, 155,  25],'deg'), 16.7, tol)
+assert_similar (average_angle([155,  25,  50],'deg'), 16.7, tol)
+
+# Test negative numbers:
+#25 = 205 = -335
+assert_similar (average_angle([-335,   50, 155],'deg'), 16.7, tol)
+assert_similar (average_angle([50,  155, -335],'deg'), 16.7, tol)
+assert_similar (average_angle([155, -335, 230],'deg'), 16.7, tol)
+assert_similar (average_angle([-335, 230, 335],'deg'), 16.7, tol)
+assert_similar (average_angle([230, 335,  -335],'deg'), 16.7, tol)
+assert_similar (average_angle([335,  -335,  50],'deg'), 16.7, tol)
+
+# 50 = 230 = -130
+assert_similar (average_angle([25,   -130, 155],'deg'), 16.7, tol)
+assert_similar (average_angle([-130,  155, 205],'deg'), 16.7, tol)
+assert_similar (average_angle([155, 205, -130],'deg'), 16.7, tol)
+assert_similar (average_angle([205, -130, 335],'deg'), 16.7, tol)
+assert_similar (average_angle([-130, 335,  25],'deg'), 16.7, tol)
+assert_similar (average_angle([335,  25,  -130],'deg'), 16.7, tol)
+
+# 335 = 155 = -25
+assert_similar (average_angle([25,   50, -25],'deg'), 16.7, tol)
+assert_similar (average_angle([50,  -25, 205],'deg'), 16.7, tol)
+assert_similar (average_angle([-25, 205, 230],'deg'), 16.7, tol)
+assert_similar (average_angle([205, 230, -25],'deg'), 16.7, tol)
+assert_similar (average_angle([230, -25,  25],'deg'), 16.7, tol)
+assert_similar (average_angle([-25,  25,  50],'deg'), 16.7, tol)
+
+# additional tests
+assert_similar (average_angle([70,  -80],'deg'), 85, tol)
+assert_similar (average_angle([-80,  70],'deg'), 85, tol)
 assert_similar (average_angle([10,20,350]),6.6666,tol)
 assert_similar (average_angle([10,20,180,180,340]),2.0,tol)
 assert_similar (average_angle([15,355,25,-15]),5.0,tol)
@@ -44,4 +112,4 @@ print ("All tests passed!")
 
 # example
 list_angles = [15,175,25,-15]
-print("average_angle (15,175,25,-15) = ",average_angle(list_angles))
+print("Example: average_angle (15,175,25,-15) = ",average_angle(list_angles))
